bb59f94afc1205bc4be92084dd20c39802a39aa6
Matt Sinclair <mattdsinclair@gmail.com>
2020-06-03 00:34:53 -0500

    Change URL format to conform to new DBLP approach

--- dblp/__init__.py
+++ dblp/__init__.py
@@ -6,7 +6,8 @@ DBLP_BASE_URL = 'http://dblp.uni-trier.de/'
 DBLP_AUTHOR_SEARCH_URL = DBLP_BASE_URL + 'search/author'
 
 DBLP_PERSON_URL = DBLP_BASE_URL + 'pers/xk/{urlpt}'
-DBLP_PUBLICATION_URL = DBLP_BASE_URL + 'rec/bibtex/{key}.xml'
+# per DBLP staff, rec/conf/... instead of rec/bibtex/conf/... is the "new" format
+DBLP_PUBLICATION_URL = DBLP_BASE_URL + 'rec/{key}.xml'
 
 class LazyAPIData(object):
     def __init__(self, lazy_attrs):
